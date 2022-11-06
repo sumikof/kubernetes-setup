0d58fb0d7262b59fafca767c14d3c65281e353e4
sumikof <sumikof@gmail.com>
2022-11-06 07:18:34 +0000

    kubernetes version 1.24

--- roles/kube-master/files/init.sh
+++ roles/kube-master/files/init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo kubeadm init --control-plane-endpoint=192.168.10.10:6443 --pod-network-cidr=10.244.0.0/16
+sudo kubeadm init --control-plane-endpoint=kube-api:6443 --pod-network-cidr=10.244.0.0/16
--- roles/kube-master/files/kube-config-setup.sh
+++ roles/kube-master/files/kube-config-setup.sh
@@ -2,3 +2,5 @@
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+source ~/.bashrc
